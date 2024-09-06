fd94d0b7eb60720288e6d8ca21d6bb5232e98a09
D4RK4Tv2 <darkat.1337@gmail.com>
2024-09-06 20:14:05 +0200

    Add files via upload

---
+++ SimpleArkaAPI/SimpleArkaAPI/ArkaAPI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleArkaAPI
+{
+    public class ArkaAPI
+    {
+        static StreamReader reader;
+        static StreamWriter writer;
+        static NamedPipeClientStream client;
+        static string pipeName = "arka";
+
+        public static void Init()
+        {
+            client = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut);
+            reader = new StreamReader(client);
+            writer = new StreamWriter(client);
+
+            StartInjector();
+            client.Connect();
+            writer.AutoFlush = true;
+            writer.WriteLine("auth:" + Process.GetCurrentProcess().Id);
+        }
+
+        public static string Inject()
+        {
+            writer.WriteLine("inject");
+            string response = reader.ReadLine();
+            return response;
+        }
+
+        public static void Execute(string script)
+        {
+            File.WriteAllText("temp.lua", script);
+            writer.WriteLine("exec:temp.lua");
+        }
+
+        private static void StartInjector()
+        {
+            if (!File.Exists("ArkaInjectorV3.exe"))
+            {
+                MessageBox.Show("injector is missing");
+                return;
+            }
+
+            Process.Start("ArkaInjectorV3.exe");
+        }
+    }
+}
---
+++ SimpleArkaAPI/SimpleArkaAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleArkaAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArkaAPI.Init();
+            Thread.Sleep(5000);
+            ArkaAPI.Inject();
+            Console.ReadKey();
+        }
+    }
+}
